9715b6af8433b3327bad8b2d33fcd0d09d35baf2
Alexei Pesic <aly.pesic@gmail.com>
2014-09-21 15:59:19 -0400

    Catch soundcloud error and retry to prevent playlist failure

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,22 +1,28 @@
+require 'pry'
+
 class Track < ActiveRecord::Base
   has_many :playlist_tracks
   has_many :playlists, :through => :playlist_tracks
 
   def get_soundcloud_uri
-    client = soundcloud_client
+    retries = 2
     begin
-      sc_tracks = client.get(
-        '/tracks',
-        :q => "#{artist} #{title}",
-        :limit=>20,
-        :duration => {:from => 120000, :to => 600000},
-        :streamable => true,
-      )
+      sc_tracks = sc_search
     rescue
-      binding.pry
-      return false
+      if retries > 0
+        retries -= 1
+        sleep 1
+        retry
+      else
+        return false
+      end
     end
-    top_track = get_sc_top_track(sc_tracks)
+    get_sc_top_track(sc_tracks)
+  end
+
+  def get_sc_top_track(track_list)
+    track_list.reject! {|track| track.playback_count.nil? }
+    top_track = track_list.sort_by {|track| track.playback_count}.reverse.first
     if top_track
       update(sc_uri: top_track['uri'])
       return true
@@ -25,9 +31,14 @@ class Track < ActiveRecord::Base
     end
   end
 
-  def get_sc_top_track(track_list)
-    track_list.reject! {|track| track.playback_count.nil? }
-    return track_list.sort_by {|track| track.playback_count}.reverse.first
+  def sc_search
+    soundcloud_client.get(
+      '/tracks',
+      :q => "#{artist} #{title}",
+      :limit=>20,
+      :duration => {:from => 120000, :to => 600000},
+      :streamable => true,
+    )
   end
 
   def soundcloud_client
